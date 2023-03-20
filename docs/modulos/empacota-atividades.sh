2e27a93568c9c45c5dced08086037012adcbcd9c
Igor Montagner <igordsm@gmail.com>
2023-03-20 09:23:01 -0300

    Módulo 3 - visão computacional

--- docs/modulos/empacota-atividades.sh
+++ docs/modulos/empacota-atividades.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-pushd 01-imagens-e-matrizes
-zip atividades-modulo01-aluno.zip *ipynb img/*
-popd
+# pushd 01-imagens-e-matrizes
+# zip atividades-modulo01-aluno.zip *ipynb img/*
+# popd
 
-pushd 02-modelo-de-camera
-zip atividades-modulo02-aluno.zip *ipynb auxiliar.py img/*
+# pushd 02-modelo-de-camera
+# zip atividades-modulo02-aluno.zip *ipynb auxiliar.py img/*
+# popd
+
+pushd 03-visao
+zip atividades-modulo03-aluno.zip *ipynb *.py img/* *jpg
 popd
