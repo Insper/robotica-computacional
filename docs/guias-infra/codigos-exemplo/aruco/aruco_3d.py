55be0c80079737a49fc39b4d28632a8a2722e617
LaizFarias <81171500+LaizFarias@users.noreply.github.com>
2023-04-28 10:47:23 -0300

    Update aruco.md (#15)
    * Update aruco.md
    
    * Update aruco.md
    
    * Update aruco.md
    
    * Update aruco_3d.py
    
    * Update aruco_3d.py
    
    * Update aruco.md
    
    * Update aruco_3d.py
    
    * Update aruco_3d.py
    
    * Update aruco_3d.py
    
    * Update aruco_3d.py
    
    ---------
    
    Co-authored-by: Licia Sales <40764768+liciascl@users.noreply.github.com>

--- docs/guias-infra/codigos-exemplo/aruco/aruco_3d.py
+++ docs/guias-infra/codigos-exemplo/aruco/aruco_3d.py
@@ -1,111 +1,114 @@
-#! /usr/bin/env python3
-# -*- coding:utf-8 -*-
-
-import rospy
-import numpy as np
-import math
+#importando as bibliotecas utilizadas
+import cv2.aruco as aruco
 import cv2
-import time
-from geometry_msgs.msg import Twist, Vector3, Pose
+import numpy as np
+from geometry_msgs.msg import Twist 
 from sensor_msgs.msg import Image, CompressedImage, LaserScan
 from cv_bridge import CvBridge, CvBridgeError
+import rospy
 import os
 
-import cv2.aruco as aruco
-import sys
+#definindo a classe Aruco3d
+class Aruco3d:
+    def __init__(self):
+            
+            self.bridge = CvBridge()
+            #Criando o callback do topico da camera do robo, para receber cada frame transmitido
+            self.image_sub = rospy.Subscriber('/camera/image/compressed',CompressedImage,self.image_callback,queue_size=1,buff_size = 2**24)
+            
+            #Definindo centros_aruco para armazenar as coordenadas [x,y] do ponto central do aruco detectado
+            self.centros_aruco = []
+            #Definindo a variavel ids para armazenar o Id do aruco detectado
+            self.ids = None 
+            #Definindo a lista que vai armazenar os pontos contendo a distancia dos arucos detectados em relacao ao robo
+            self.distancias = []
 
-#--- Define Tag de teste
-id_to_find  = 200
-marker_size  = 20 #- [cm]
+            #Capturando o caminho da pasta local em que o codigo esta
+            calibra_path  = os.path.abspath(os.getcwd())
+            
+            # =================================== APENAS PARA O ROBO FISICO ===========================================================
+            # ========================================= IMPORTANTE!!!! ================================================================
+            # ===== Se estiver usando o robo fisico, e o seu robo está com a RealSense, troque o arquivo de calibração =====
+            # ===== Verifique se o arquivo cameraDistortion_realsense.txt e cameraMatrix_realsense.txt esta disponivel na pasta arucopath ====
+            # ===== Troque o nome do arquivo nas variaveis camera_matrix e camera_distortion para cameraDistortion_realsense.txt e cameraMatrix_realsense.txt respectivamente. 
+            # ===== SE O SEU ROBO ESTA USANDO A RASPICAM, NAO PRECISA ALTERAR NADA!
+            # ===== Caso não saiba do que eu estou falando, por favor, jogue no google imagens "Camera RealSense D400", essa é a camera RealSense.
+            
+            
+            #Carregando os arquivos de calibracao da camera
+            #Exemplo de calibracao para a raspcam, funciona no robo simulado e no robo real que está com a Raspcam
+            self.camera_matrix   = np.loadtxt(calibra_path+'/arucopath/cameraMatrix_raspi.txt', delimiter=',')
+            self.camera_distortion   = np.loadtxt(calibra_path+'/arucopath/cameraDistortion_raspi.txt', delimiter=',')
 
-#--- Get the camera calibration path
-calib_path  = os.path.abspath(os.getcwd())
-camera_matrix   = np.loadtxt(calib_path+'/cameraMatrix_realsense.txt', delimiter=',')
-camera_distortion   = np.loadtxt(calib_path+'/cameraDistortion_realsense.txt', delimiter=',')
+    def image_callback(self, msg: CompressedImage) -> None:
+        #converte a msg do ROS para OpenCV 
+        cv_image = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+        #chama a função de leitura dos Arucos
+        self.ids, self.centros_aruco, self.distancias =self.geraAruco(cv_image)
+        #carrega uma copia da imagem ja decodificada para a variavel img
+        img = cv_image.copy()      
 
-#--- Define the aruco dictionary
-aruco_dict  = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
-parameters  = aruco.DetectorParameters_create()
-parameters.minDistanceToBorder = 0
-parameters.adaptiveThreshWinSizeMax = 1000
-
-#-- Font for the text in the image
-font = cv2.FONT_HERSHEY_PLAIN
-
-#converte a msg do ROS para OpenCV
-bridge = CvBridge() 
-cv_image = None
-scan_dist = 0
-
-def scaneou(dado):
-	scan_dist = dado.ranges[0]*100
-	return scan_dist
-
-
-# A função a seguir é chamada sempre que chega um novo frame
-def roda_todo_frame(imagem):
-    # Recebe o frame comprimido via ROS
-    cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8") 
-    #Converte a imagem pra tons de cinza
-    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-    #Cria uma lista de pontos e ids diponiveis
-    corners, ids, pontosdescartaveis = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-    #exibe a lista de ids
-    print(ids)
-
-    #Se a lista de ids nao estiver vazia
-    if ids is not None:
-
-        #-- ret recebe os dados de posicao, rvec e tvec 
-        ret = aruco.estimatePoseSingleMarkers(corners, marker_size, camera_matrix, camera_distortion)
-
-        #-- rvec e uma lista contendo os vetores de rotacao  -----> rvec = [[rvec_1], [rvec_2], ...] 
-        #-- tvec e uma lista contendo os vetores de translação ---------> tvec = [[tvec_1], [tvec_2], ...] vetor de translação
-        rvec, tvec = ret[0][0,0,:], ret[1][0,0,:]
-
-        #-- Desenha um retanculo e exibe Id do marker encontrado
-        aruco.drawDetectedMarkers(cv_image, corners, ids) 
-        cv2.drawFrameAxes(cv_image, camera_matrix, camera_distortion, rvec, tvec ,0.03)
-
-
-
-        #-- Exibe tvec --> vetor de tanslação em x y z
-        str_position = "Marker x=%4.0f  y=%4.0f  z=%4.0f"%(tvec[0], tvec[1], tvec[2])
-        print(str_position)
-        #-- Desenha na imagem o vetor de tanslação em x y z
-        cv2.putText(cv_image, str_position, (0, 100), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
-
-        ##############----- Referencia dos Eixos------###########################
-        # Desenha a linha referencia em X
-        cv2.line(cv_image, (cv_image.shape[1]//2,cv_image.shape[0]//2), ((cv_image.shape[1]//2 + 50),(cv_image.shape[0]//2)), (0,0,255), 5) 
-        # Desenha a linha referencia em Y
-        cv2.line(cv_image, (cv_image.shape[1]//2,cv_image.shape[0]//2), (cv_image.shape[1]//2,(cv_image.shape[0]//2 + 50)), (0,255,0), 5) 	
+    def geraAruco(self,cv_image):
+        centros = []
+        distancia_aruco=[]
+        # Gera a mascara em escalas de Cinza apartir da copia da imagem la da função image_callback
+        grayColor = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #Define o Dicionario para o Aruco que vamos utilizar
+        dicionarioAruco = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
         
-        #####################---- Distancia Euclidiana ----#####################
-        # Calcula a distancia usando apenas a matriz tvec, matriz de tanslação
-        distance = np.sqrt(tvec[0]**2 + tvec[1]**2 + tvec[2]**2)
-        distancenp = np.linalg.norm(tvec)
+        #Detecta os Arucos e Carrega os pontos de translação e de rotação do aruco em relação ao robo
+        cornersList, ids, _ = aruco.detectMarkers(
+            grayColor, dicionarioAruco)
+        # Se um Id foi detectado, verifica se ele esta dentro da range de 0 a 99 e calcula os valores de rotação e translação 
+        if ids is not None:
+            for i in range(len(ids)):
+                if ids[i]>99:
+                    ret = aruco.estimatePoseSingleMarkers(cornersList[i], 19, self.camera_matrix, self.camera_distortion)
+                    rvec, tvec = ret[0][0,0,:], ret[1][0,0,:]
+                    distancia_aruco.append(np.linalg.norm(tvec))
+                else: 
+                    ret = aruco.estimatePoseSingleMarkers(cornersList[i], 6, self.camera_matrix, self.camera_distortion)
+                    rvec, tvec = ret[0][0,0,:], ret[1][0,0,:]
+                    distancia_aruco.append(np.linalg.norm(tvec))
+                
+            #Captura as coordenadas dos cantos do Aruco detectado
+            for corners in cornersList:
+                for corner in corners:
+                    centros.append(np.mean(corner, axis=0))
 
-        #-- Print distance
-        str_dist = "Dist aruco=%4.0f  dis.np=%4.0f"%(distance, distancenp)
-        print(str_dist)
-        cv2.putText(cv_image, str_dist, (0, 15), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
+            #-- Desenha um retanculo e exibe Id do marker encontrado
+            aruco.drawDetectedMarkers(cv_image, cornersList, ids) 
+            cv2.drawFrameAxes(cv_image, self.camera_matrix, self.camera_distortion, rvec, tvec ,0.03)
+            #-- Exibe tvec --> vetor de tanslação em x y z
+            str_position = "Marker x=%4.0f  y=%4.0f  z=%4.0f"%(tvec[0], tvec[1], tvec[2])
+            #-- Desenha na imagem o vetor de tanslação em x y z
+            cv2.putText(cv_image, str_position, (0, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
+            # Desenha a linha referencia em X
+            cv2.line(cv_image, (cv_image.shape[1]//2,cv_image.shape[0]//2), ((cv_image.shape[1]//2 + 50),(cv_image.shape[0]//2)), (0,0,255), 5) 
+            # Desenha a linha referencia em Y
+            cv2.line(cv_image, (cv_image.shape[1]//2,cv_image.shape[0]//2), (cv_image.shape[1]//2,(cv_image.shape[0]//2 + 50)), (0,255,0), 5) 
+
+            # Exibe a imagem na tela
+            cv2.imshow("Camera", cv_image)
+            cv2.waitKey(1)
+
+        #retorna os ids e as coordenadas de centro e de distancia do aruco em relação ao robo
+        return ids, centros, distancia_aruco
 
 
-    # Exibe tela
-    cv2.imshow("Camera", cv_image)
-    cv2.waitKey(1)
-
-	
-if __name__=="__main__":
-
+       
+    def main(self):
+        print('id {}\n\ncentro{}\n\ndistancia{}'.format(self.ids,self.centros_aruco,self.distancias))
+            
+                    
+if __name__ == "__main__":
     #inicializa o node de conexao com o ROS
-	rospy.init_node("aruco")
+    rospy.init_node("Aruco")
+    #inicializa a classe Aruco3d
+    arucos = Aruco3d()
 
-    #Chama a função roda_todo_frame sempre que chegar um frame novo
-	recebe_imagem = rospy.Subscriber("/camera/image/compressed" , CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
-
-    #Loop do ROS
-	while not rospy.is_shutdown():
-		rospy.sleep(0.1)
+    
+    while not rospy.is_shutdown():
+        # Chama a função main da classe Aruco3d
+        arucos.main()
 
