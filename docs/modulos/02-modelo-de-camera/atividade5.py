fe2508bd8ebd3e074742bf0ee320005d14b784f1
Diego <dpsoler09@gmail.com>
2023-08-25 14:33:26 -0300

    m2a5

--- docs/modulos/02-modelo-de-camera/atividade5.py
+++ docs/modulos/02-modelo-de-camera/atividade5.py
@@ -2,33 +2,46 @@ from module import ImageModule
 import cv2
 import numpy as np
 
-class Atividade5(ImageModule): # TODO adicionar comentario para criar Herança
+class Atividade5(): # Essa classe deve herdar da classe ImageModule
     def __init__(self):
-        super().__init__()
+        pass # Podem apagar o pass depois de implementar a classe
+
+        # Inicializar a classe pai
+
         # Definir aqui os atributos da classe
     
-    def run(self):
+    def run(self, bgr: np.ndarray) -> np.ndarray:
+        """Esta função deve ser processar o frame de entrada chamando as funções necessárias para executar a atividade 5. 
+        Crie quantas funções auxiliares achar necessário dentro dessa classe ou dentro da classe ImageModule.
+
+        Se desejar pode retornar uma máscara durante o desenvolvimento para facilitar a visualização
+
+        Args:
+            bgr (np.ndarray): Frame de entrada
+
+        Returns:
+            bgr (np.ndarray): Frame com a atividade 5 desenhada
+        """
         # Esta função deve ser implementada para executar a atividade 5
         pass
 
 def rodar_frame():
     RodaAtividade = Atividade5()
     
-    bgr = cv2.imread("img/frame01.jpg")
+    bgr = cv2.imread("img/frame01.jpg") # Escolha aqui a imagem que deseja usar para testar
     # bgr = cv2.imread("img/frame02.jpg")
     # bgr = cv2.imread("img/frame03.jpg")
 
-    brg = RodaAtividade.run(bgr)
+    img = RodaAtividade.run(bgr)
 
-    cv2.imshow("Imagem", bgr)
-    # cv2.imshow("Mascara", mask)
+    cv2.imshow("Imagem", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 def rodar_video():
     RodaAtividade = Atividade5()
 
-    cap = cv2.VideoCapture('img/video.mp4')
+    cap = cv2.VideoCapture('img/video.mp4') # Confira se o video esta na pasta img
 
     while(cap.isOpened()):
         ret, frame = cap.read()
@@ -43,7 +56,7 @@ def rodar_video():
             
 
 if __name__ == "__main__":
-    # Selecione se deseja rodar seu codigo com a imagem ou um video:
+    # Selecione se deseja rodar seu codigo com uma imagem ou um video:
 
     rodar_frame()
     # rodar_video()
