5b8b688e15f20d62d5fafa6d4f83e1f26e5ad5de
Diego <dpsoler09@gmail.com>
2023-09-04 22:29:23 -0300

    docstring

--- docs/modulos/03-visao/atividade3.py
+++ docs/modulos/03-visao/atividade3.py
@@ -6,9 +6,22 @@ from module_net import MobileNetDetector
 class DangerDetector(MobileNetDetector):
     def __init__(self, CONFIDENCE=0.7):
         super().__init__()
+        # Ajuste o valor de confiança para o valor que você achar melhor
         self.CONFIDENCE = CONFIDENCE
         
-    def separar_caixa_entre_animais(self, img, resultados):
+    def separar_caixa_entre_animais(self, img: np.ndarray, resultados: list) -> (np.ndarray, dict):
+        """Não mude ou renomeie esta função
+        Combina as caixas dos lobos em uma unica grande caixa e organiza as caixas das vacas em uma lista.
+        Isso facilitará a verificação de perigo para cada vaca.
+
+        Args:
+            img (np.ndarray): Imagem de entrada
+            resultados (list): Lista com as detecções no formato [classe, confiança, (xmin, ymin), (xmax, ymax)]
+
+        Returns:
+            img: Imagem de saída
+            animais: Dicionário com as caixas dos animais no formato {'vaca': [[(xmin, ymin, xmax, ymax)],[...],...], 'lobo': [xmin, ymin, xmax, ymax]}
+        """
         img = img.copy()
         animais = {'vaca': [], 'lobo': []}
         for i in range(len(resultados)):
@@ -47,9 +60,15 @@ class DangerDetector(MobileNetDetector):
 
         return img, animais
 
-    def calcula_iou(self, boxA, boxB):
-        """Não mude ou renomeie esta função
-            Calcula o valor do "Intersection over Union" para saber se as caixa se encontram
+    def calcula_iou(self, boxA: list, boxB: list) -> float:
+        """Calcula a intersecção sobre a união (Intersection over Union - IoU) entre duas caixas.
+
+        Args:
+            boxA (list): Caixa 1 no formato: [xmin, ymin, xmax, ymax]
+            boxB (list): Caixa 2 no formato: [xmin, ymin, xmax, ymax]
+
+        Returns:
+            iou: Retorna o valor da IoU entre as duas caixas
         """
         xA = min(boxA[0], boxB[0])
         yA = min(boxA[1], boxB[1])
@@ -69,13 +88,20 @@ class DangerDetector(MobileNetDetector):
 
         return iou
 
-    def checar_perigo(self, image, animais):
+    def checar_perigo(self, image: np.ndarray, animais: dict) -> np.ndarray:
         """Não mude ou renomeie esta função
             Recebe as coordenadas das caixas, se a caixa de uma vaca tem intersecção com as do lobo, ela esta em perigo.
             Se estiver em perigo, deve escrever na imagem com a cor vermlha, se não, escreva com a cor azul.
             *Importante*: nesta função, não faça cópia da imagem de entrada!!
             
             Repita para cada vaca na imagem.
+
+        Args:
+            image (np.ndarray): Imagem de entrada
+            animais (dict): Dicionário com as caixas dos animais no formato {'vaca': [[(xmin, ymin, xmax, ymax)],[...],...], 'lobo': [xmin, ymin, xmax, ymax]}
+
+        Returns:
+            image: Imagem de saída com as caixas desenhadas e o texto indicando se está em perigo ou não
         """
 
         for i in animais['vaca']:
--- docs/modulos/03-visao/module_net.py
+++ docs/modulos/03-visao/module_net.py
@@ -8,6 +8,8 @@ import os
 
 
 class MobileNetDetector():
+    """Classe para detecção de objetos com o modelo MobileNetSSD.
+    """
     def __init__(self,
                  CONFIDENCE = 0.7,
                  args_prototxt = "./config/MobileNetSSD_deploy.prototxt.txt",
@@ -16,114 +18,56 @@ class MobileNetDetector():
         self.CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
         self.CONFIDENCE = CONFIDENCE
         self.COLORS = np.random.uniform(0, 255, size=(len(self.CLASSES), 3))
-        
+
         self.args_prototxt = args_prototxt
         self.args_model = args_model
         self.net = self.load_mobilenet()
 
     def load_mobilenet(self):
+        """Carrega o modelo MobileNetSSD.
+        Certifique-se de que os arquivos .prototxt.txt e .caffemodel diretório correto.
+
+        Returns:
+            net: modelo carregado
+        """
         net = cv2.dnn.readNetFromCaffe(self.args_prototxt, self.args_model)
         return net
 
-    def detect(self, frame):
+    def detect(self, frame: np.ndarray):
+        """Detecta objetos na imagem de entrada.
+        Filtra as detecções com uma confiança menor que self.CONFIDENCE.
+
+        Args:
+            frame (np.ndarray): Imagem de entrada
+
+        Returns:
+            image: Imagem de saída com as detecções desenhadas
+            results: Lista com as detecções no formato [classe, confiança, (xmin, ymin), (xmax, ymax)]
+        """
         image = frame.copy()
         (h, w) = image.shape[:2]
         blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
         self.net.setInput(blob)
         detections = self.net.forward()
+        
         results = []
         for i in np.arange(0, detections.shape[2]):
             confidence = detections[0, 0, i, 2]
+
             if confidence > self.CONFIDENCE:
                 idx = int(detections[0, 0, i, 1])
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype("int")
+
                 label = "{}: {:.2f}%".format(self.CLASSES[idx], confidence * 100)
+
                 cv2.rectangle(image, (startX, startY), (endX, endY), self.COLORS[idx], 2)
                 y = startY - 15 if startY - 15 > 15 else startY + 15
                 cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.COLORS[idx], 2)
+
                 results.append((self.CLASSES[idx], confidence*100, (startX, startY), (endX, endY)))
+
         return image, results
-
-    def separar_caixa_entre_animais(self, img, resultados):
-        img = img.copy()
-        animais = {'vaca': [], 'lobo': []}
-        for i in range(len(resultados)):
-            if resultados[i][0] == 'cow':
-                cv2.rectangle(img, (resultados[i][2][0], resultados[i][2][1]), (resultados[i][3][0], resultados[i][3][1]), [255,0,0], 2)
-                animais['vaca'].append([resultados[i][2][0], resultados[i][2][1], resultados[i][3][0], resultados[i][3][1]])
-            else:
-                animais['lobo'].append([resultados[i][2][0], resultados[i][2][1], resultados[i][3][0], resultados[i][3][1]])
-
-        retang_lobo = []
-        if len(animais['lobo']) == 2:
-            if animais['lobo'][0][0] > animais['lobo'][1][0]: #pegando o menor xmin
-                retang_lobo.append(animais['lobo'][1][0])
-            else:
-                retang_lobo.append(animais['lobo'][0][0])
-
-            if animais['lobo'][0][1] > animais['lobo'][1][1]: #pegando o menor ymin
-                retang_lobo.append(animais['lobo'][1][1])
-            else:
-                retang_lobo.append(animais['lobo'][0][1])
-
-            if animais['lobo'][0][2] > animais['lobo'][1][2]: #pegando o maior xmax
-                retang_lobo.append(animais['lobo'][0][2])
-            else:
-                retang_lobo.append(animais['lobo'][1][2])
-
-            if animais['lobo'][0][3] > animais['lobo'][1][3]: #pegando o maior ymax
-                retang_lobo.append(animais['lobo'][0][3])
-            else:
-                retang_lobo.append(animais['lobo'][1][3])
-        
-            animais['lobo'] = retang_lobo
-            cv2.rectangle(img, (retang_lobo[0], retang_lobo[1]),(retang_lobo[2], retang_lobo[3]), [0,0,255], 2)
-        else:
-            cv2.rectangle(img, (animais['lobo'][0][0], animais['lobo'][0][1]), (animais['lobo'][0][2], animais['lobo'][0][3]), [0,0,255], 2)
-
-        return img, animais
-
-    def calcula_iou(self, boxA, boxB):
-        """Não mude ou renomeie esta função
-            Calcula o valor do "Intersection over Union" para saber se as caixa se encontram
-        """
-
-        xA = min(boxA[0], boxB[0])
-        yA = min(boxA[1], boxB[1])
-        xB = max(boxA[2], boxB[2])
-        yB = max(boxA[3], boxB[3])
-
-        # compute the area of intersection rectangle
-        interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-        # compute the area of both the prediction and ground-truth
-        # rectangles
-        boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-        boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-        # compute the intersection over union by taking the intersection
-        # area and dividing it by the sum of prediction + ground-truth
-        # areas - the interesection area
-        iou = interArea / float(boxAArea + boxBArea - interArea)
-
-        return iou
-
-    def checar_perigo(self, image, animais):
-        """Não mude ou renomeie esta função
-            Recebe as coordenadas das caixas, se a caixa de uma vaca tem intersecção com as do lobo, ela esta em perigo.
-            Se estiver em perigo, deve escrever na imagem com a cor vermlha, se não, escreva com a cor azul.
-            *Importante*: nesta função, não faça cópia da imagem de entrada!!
-            
-            Repita para cada vaca na imagem.
-        """
-
-        for i in animais['vaca']:
-            iou = self.calcula_iou(animais['lobo'][0], i)
-            if iou > 0.5:
-                image = cv2.putText(image,"Esta em perigo",(i[0], i[1]),cv2.FONT_HERSHEY_SIMPLEX,1.5,[0,0,255],3)
-            else:
-                image = cv2.putText(image,'Esta sem perigo', (i[0], i[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.5, [255, 0, 0], 3)
-            
-        return image
 
 
 def main():
