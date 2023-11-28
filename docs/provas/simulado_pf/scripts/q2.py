a9f23aa7caa42585f69c1f3fd9ea82fd40b3db91
Diego <dpsoler09@gmail.com>
2023-11-27 21:47:30 -0300

    sim pf

---
+++ docs/provas/simulado_pf/scripts/q2.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist
+
+""" 
+	roslaunch my_simulation encaixotado.launch
+	rosrun pf-robcomp q2.py
+"""
+
+class Questao2():
+	def __init__(self):
+
+		self.rate = rospy.Rate(250) # 250 Hz
+		
+		# Subscribers
+
+        # Publishers
+		self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist)
+
+
+		# Maquina de estados
+		self.robot_state = ""
+		self.robot_machine = {
+			"": self.nada
+		}
+	
+	def nada(self):
+		pass
+	
+	def control(self) -> None:
+		'''
+		This function is called at least at {self.rate} Hz.
+		This function controls the robot.
+		'''
+		
+		self.twist = Twist()
+		print(f'self.robot_state: {self.robot_state}')
+		self.robot_machine[self.robot_state]()
+
+		self.cmd_vel_pub.publish(self.twist)
+
+		self.rate.sleep()
+
+
+def main():
+	rospy.init_node('q2')
+	control = Questao2()
+	rospy.sleep(1)
+
+	while not rospy.is_shutdown():
+		control.control()
+
+if __name__=="__main__":
+	main()
