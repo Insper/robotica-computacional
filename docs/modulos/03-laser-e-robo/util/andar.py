b23b5f938a8d2c695e13ed5a929db3db8c21852e
Diego <dpsoler09@gmail.com>
2025-08-12 15:06:06 -0300

    improvig

--- docs/modulos/03-laser-e-robo/util/andar.py
+++ docs/modulos/03-laser-e-robo/util/andar.py
@@ -2,69 +2,57 @@ import rclpy
 import numpy as np
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from robcomp_util.odom import Odom
-from robcomp_util.laser import Laser
 
-class Andar(Node, Odom): # Mude o nome da classe
+class Andar(Node,): # Mude o nome da classe
 
     def __init__(self):
         super().__init__('andar_node') # Mude o nome do nó
-        # Herda de Odom para obter funcionalidades de odometria
-        Odom.__init__(self)
-
-        # Placeholder para o timer
         self.timer = None
 
-        # Definindo a maquina de estados
-        self.robot_state = 'stop'
+        self.robot_state = 'done'
         self.state_machine = {
             'andar': self.andar,
-            'stop': self.stop
+            'stop': self.stop,
+            'done': self.stop
         }
 
         # Inicialização de variáveis
         self.velocidade = 0.2
 
         # Publishers
+        # Criação do publisher para o tópico que controla a velocidade do robô
         self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
     
-    def stop(self):
-        # Zera a velocidade linear e angular
-        self.twist = Twist()
-        print("Andar: Parando o robô.")
-        self.timer.cancel()
-        self.timer = None
-        self.done = True
-
     def reset(self):
-        """Reseta o estado do robô e inicializa o timer."""
-        # Zera a velocidade linear e angular
         self.twist = Twist()
-        # Para indicar que a ação não foi concluída
-        self.done = False
-        # Muda para o estado inicial
         self.robot_state = 'andar'
-        # Inicia o timer
-        self.timer = self.create_timer(0.25, self.control)
-        # Em seguida, inicia variaveis para realizar a ação
-        # ...
+        if self.timer is None:
+            self.timer = self.create_timer(0.25, self.control)
+        ### Iniciar variaveis da ação
+        # Tempo inicial
         self.tempo_inicial = self.get_clock().now().to_msg()
         self.tempo_inicial = float(self.tempo_inicial.sec)
 
     def andar(self):
-        # Robo deve andar pra frente por 4 segundos
-        # e depois parar
-        # ...
+        # Definar a velocidade linear do robô na variável twist
         self.twist.linear.x = self.velocidade
+        # Recolher o tempo atual
         self.tempo_atual = self.get_clock().now().to_msg()
-        self.tempo_atual = float(self.tempo_atual.sec) 
+        self.tempo_atual = float(self.tempo_atual.sec)
+        # Calcular o delta de tempo
         delta = self.tempo_atual - self.tempo_inicial
         print(f'Delta: {delta} segundos')
-
+        # Se o delta é maior ou igual a t segundos, pare o robô
         if delta >= 4.0:
                 self.twist.linear.x = 0.0
                 self.robot_state = 'stop'
 
+    def stop(self):
+        self.twist = Twist()
+        print("Parando o robô.")
+        self.timer.cancel()
+        self.timer = None
+        self.robot_state = 'done'
 
     def control(self):
         print(f'Estado Atual: {self.robot_state}')
@@ -72,18 +60,13 @@ class Andar(Node, Odom): # Mude o nome da classe
         self.cmd_vel_pub.publish(self.twist)
  
 def main(args=None):
-    # Inicia o nó ROS2
     rclpy.init(args=args)
-    # Cria uma instância do nó
     ros_node = Andar()
 
-    # Ros2 spin_once para processar callbacks
-    # Para capturar informações iniciais do Odom e Laser por exemplo
     rclpy.spin_once(ros_node)
-    # Reset the node to initialize the goal yaw
     ros_node.reset()
 
-    while not ros_node.robot_state == 'stop':
+    while not ros_node.robot_state == 'done':
         rclpy.spin_once(ros_node)
 
     ros_node.destroy_node()
--- docs/modulos/03-laser-e-robo/util/base_action.py
+++ docs/modulos/03-laser-e-robo/util/base_action.py
@@ -2,80 +2,68 @@ import rclpy
 import numpy as np
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from robcomp_util.odom import Odom
-from robcomp_util.laser import Laser
 
-class BaseAction(Node): # Mude o nome da classe
+class Acao(Node,): # Mude o nome da classe
 
     def __init__(self):
-        super().__init__('action_node') # Mude o nome do nó
-
-        # Placeholder para o timer
+        super().__init__('acao_node') # Mude o nome do nó
         self.timer = None
 
-        # Definindo a maquina de estados
-        self.robot_state = 'stop'
-        self.state_machine = {
+        self.robot_state = 'done' # Comece em 'done' - reset iniciará a ação
+        self.state_machine = { # Adicione quantos estados forem necessários
             'acao': self.acao,
-            'stop': self.stop
+            'stop': self.stop,
+            'done': self.stop
         }
 
         # Inicialização de variáveis
-        # ... (se necessário)
+        # ...
 
         # Publishers
         self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
+
+        # Subscribers
+        # ...
     
     def reset(self):
-        """Reseta o estado do robô e inicializa o timer."""
-        # Zera a velocidade linear e angular
-        self.twist = Twist() 
-        # Indica que a ação não foi concluída
-        self.done = False 
-        # Muda para o estado inicial
-        self.robot_state = 'acao'
-        # Inicia o timer
-        self.timer = self.create_timer(0.25, self.control)
-
-        # Em seguida, inicia variaveis para realizar a ação
-        # ... (se necessário)
+        self.twist = Twist()
+        self.robot_state = 'acao' # Inicie a ação
+        if self.timer is None:
+            self.timer = self.create_timer(0.25, self.control) # Timer para o controle
+        ### Iniciar variaveis da ação
+        # 1. Tempo inicial
 
     def acao(self):
-        self.twist.linear.x = self.velocidade
-        self.tempo_atual = self.get_clock().now().to_msg()
-        self.tempo_atual = float(self.tempo_atual.sec) 
-        delta = self.tempo_atual - self.tempo_inicial
-        print(f'Delta: {delta} segundos')
-
-        if delta >= 4.0:
-                self.twist.linear.x = 0.0
-                self.robot_state = 'stop'
+        # 1. Definar a velocidade linear do robô na variável twist
+        # 2. Recolher o tempo atual
+        # 3. Calcular o delta de tempo
+        # 4. Se o delta é maior ou igual a t segundos, pare o robô
+        pass
 
     def stop(self):
-        self.twist = Twist()
-        print("Andar: Parando o robô.")
-        self.timer.cancel()
-        self.timer = None
-        self.done = True
+        self.twist = Twist() # Zera a velocidade
+        print("Parando o robô.")
+        self.timer.cancel() # Finaliza o timer
+        self.timer = None # Reseta a variável do timer
+        self.robot_state = 'done' # Ação finalizada
 
-    def control(self):
+    def control(self): # Controla a máquina de estados - eh chamado pelo timer
         print(f'Estado Atual: {self.robot_state}')
-        self.state_machine[self.robot_state]()     
-        self.cmd_vel_pub.publish(self.twist)
+        self.state_machine[self.robot_state]() # Chama o método do estado atual 
+        self.cmd_vel_pub.publish(self.twist) # Publica a velocidade
  
 def main(args=None):
-    rclpy.init(args=args)
-    ros_node = BaseAction()
+    rclpy.init(args=args) # Inicia o ROS2
+    ros_node = Acao() # Cria o nó
 
-    rclpy.spin_once(ros_node)
-    # Reset the node to initialize the goal yaw
-    ros_node.reset()
+    rclpy.spin_once(ros_node) # Processa as callbacks uma vez
+    ros_node.reset() # Reseta o nó para iniciar a ação
 
-    while not ros_node.robot_state == 'stop':
-        rclpy.spin_once(ros_node)
+    while not ros_node.robot_state == 'done': # Enquanto a ação não estiver finalizada
+        rclpy.spin_once(ros_node) # Processa os callbacks e o timer
 
-    ros_node.destroy_node()
-    rclpy.shutdown()
+    ros_node.destroy_node() # Destroi o nó
+    rclpy.shutdown()    # Finaliza o ROS2
 
-if __name__ == '__main__':
+if __name__ == '__main__': # Executa apenas se for o arquivo principal
     main()
